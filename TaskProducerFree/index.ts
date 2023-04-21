8d08b3da8792cf48ad8acebe7beb3d38b11d66cb
Shahid Nasrullah <shahid610169@gmail.com>
2023-04-21 11:09:24 +0500

    Queue Messages are being sent

--- TaskProducerFree/index.ts
+++ TaskProducerFree/index.ts
@@ -1,13 +1,78 @@
 import { AzureFunction, Context } from "@azure/functions"
+import { ServiceBusClient } from "@azure/service-bus";
+import { config } from 'dotenv';
+
+config({
+    path: './prod.env'
+});
 
 const timerTrigger: AzureFunction = async function (context: Context, myTimer: any): Promise<void> {
-    var timeStamp = new Date().toISOString();
-    
-    if (myTimer.isPastDue)
-    {
-        context.log('Timer function is running late!');
+
+    const connectionString = process.env.SERVICE_BUG_CONNECTION;
+    const queueName = process.env.SERVICE_BUS_FREE_QUEUE;
+
+    if (connectionString && queueName) {
+
+        // Create client
+        const sbClient = new ServiceBusClient(connectionString);
+
+        // createSender() can also be used to create a sender for a topic.
+        const sender = sbClient.createSender(queueName);
+
+        const messages = [
+            { body: "Albert Einstein" },
+            { body: "Werner Heisenberg" },
+            { body: "Marie Curie" },
+            { body: "Steven Hawking" },
+            { body: "Isaac Newton" },
+            { body: "Niels Bohr" },
+            { body: "Michael Faraday" },
+            { body: "Galileo Galilei" },
+            { body: "Johannes Kepler" },
+            { body: "Nikolaus Kopernikus" }
+        ];
+
+        try {
+            // Tries to send all messages in a single batch.
+            // Will fail if the messages cannot fit in a batch.
+            // await sender.sendMessages(messages);
+
+            // create a batch object
+            let batch = await sender.createMessageBatch();
+            for (let i = 0; i < messages.length; i++) {
+                // for each message in the array			
+
+                // try to add the message to the batch
+                if (!batch.tryAddMessage(messages[i])) {
+                    // if it fails to add the message to the current batch
+                    // send the current batch as it is full
+                    await sender.sendMessages(batch);
+                    console.log(`Sent a batch of messages to the queue: ${queueName}`);
+
+                    // then, create a new batch 
+                    batch = await sender.createMessageBatch();
+
+                    // now, add the message failed to be added to the previous batch to this batch
+                    if (!batch.tryAddMessage(messages[i])) {
+                        // if it still can't be added to the batch, the message is probably too big to fit in a batch
+                        throw new Error("Message too big to fit in a batch");
+                    }
+                }
+            }
+
+            // Send the last created batch of messages to the queue
+            await sender.sendMessages(batch);
+
+            console.log(`Sent a batch of messages to the queue: ${queueName}`);
+
+            // Close the sender
+            await sender.close();
+        } finally {
+            await sbClient.close();
+        }
+    } else {
+        context.log("Connection String or Queue Name undefined");
     }
-    context.log('Timer trigger function ran!', timeStamp);   
 };
 
 export default timerTrigger;
